c426c34d43dd3914dd804fc77e32fbe2853343c1
leesanghun <lsh93620@gmail.com>
2023-12-12 17:42:45 +0900

    페이지 오류 수정

--- react_project_2/src/components/bottomcontent.jsx
+++ react_project_2/src/components/bottomcontent.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
+
 import product from "../images/product.jpg";
-import styles from "../sass/content.module.scss";
+import styles from "../sass/content.module.scss";   
+import handleScroll from "../hooks/scrollEvent";
 
 const Bottomcontent = ()=>{
+
+    useEffect(() => {
+        handleScroll();
+
+    }, []);
+
     return(<section className={styles["bottom-content"]}>
     <div className={styles["bottom-content-wrapper"]}>
-        <div className={styles["bottom-content-title"]}>
+        <div className={styles["bottom-content-wrapper-title"]}>
             <h2>Lorem lpsum</h2>
             <p>phasellus porttitor quis dolor ac commodo. Donec in vulputate turpis</p>
         </div>
-        <div className={styles["contents-box"]}>
-            <div className={styles["contents-box-images"]}>
+        <div className={styles["bottom-content-wrapper-box"]}>
+            <div className={styles["bottom-content-wrapper-box-images"]}>
                 <img className={styles["contents-box-images-item"]} src={product} alt="임시" />
             </div>
-            <div className={styles["contents-box-text"]}>
-                <h2 className={styles["contents-box-title"]}>Vestibulum non nisi quis erat aliquet pellentesque</h2>
-                <ol className={styles["contents-box-orderlist"]}>
-                    <li>Curabitur nec consectetur augue</li>
-                    <li>id convallis nibh. Cras augue tortor, ornare vitae molestie a, mollis iaculis tellus.</li>
-                    <li>Quisque et ornare sapien.</li>  
+            <div className={styles["bottom-content-wrapper-box-text"]}>
+                <ul className={styles["bottom-content-wrapper-box-text-title"]}>
+                    <li>pharetra elit et,</li>
+                    <li>tincidunt enim.</li>
+                </ul>
+                <ol className={styles["bottom-content-wrapper-box-text-orderlist"]}>
+                    <li>Duis faucibus ullamcorper urna, sed vehicula lorem ullamcorper ut.</li>
+                    <li>Cras accumsan, lorem eu varius pellentesque.</li>
+                    <li>eget turpis. Vivamus sed dolor ligula.</li>  
                 </ol>
             </div>
         </div>
